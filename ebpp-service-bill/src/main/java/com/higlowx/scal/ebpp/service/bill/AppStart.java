8b74d52e105083a27c129355ef92054c1e8d421b
李希浩 <lixihaofree@163.com>
2021-07-09 16:48:21 +0800

    nacos clients register successfully

--- ebpp-service-bill/src/main/java/com/higlowx/scal/ebpp/service/bill/AppStart.java
+++ ebpp-service-bill/src/main/java/com/higlowx/scal/ebpp/service/bill/AppStart.java
@@ -3,13 +3,14 @@ package com.higlowx.scal.ebpp.service.bill;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author Dylan.Li
  * @date 2021/7/7
  * @since
  */
-
+@EnableDiscoveryClient
 @SpringBootApplication
 @MapperScan("com.higlowx.scal.ebpp.service.bill.mybatis.mapper")
 public class AppStart {
