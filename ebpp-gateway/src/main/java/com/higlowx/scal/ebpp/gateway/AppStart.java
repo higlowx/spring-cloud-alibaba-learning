637d20cb94c55811b2bcc88e3e0ea7f451d1e6c8
李希浩 <lixihaofree@163.com>
2021-07-12 18:25:15 +0800

    ebpp-gateway add sentinel

--- ebpp-gateway/src/main/java/com/higlowx/scal/ebpp/gateway/AppStart.java
+++ ebpp-gateway/src/main/java/com/higlowx/scal/ebpp/gateway/AppStart.java
@@ -1,10 +1,17 @@
 package com.higlowx.scal.ebpp.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author Dylan.Li
  * @date 2021/7/7
  * @since
  */
-
+@SpringBootApplication
 public class AppStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppStart.class, args);
+    }
 }
