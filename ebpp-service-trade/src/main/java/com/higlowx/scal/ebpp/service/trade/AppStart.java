157cb9eda74984cc9782f68d777904c01211a9f3
李希浩 <lixihaofree@163.com>
2021-08-03 17:14:16 +0800

    bill service use config center

--- ebpp-service-trade/src/main/java/com/higlowx/scal/ebpp/service/trade/AppStart.java
+++ ebpp-service-trade/src/main/java/com/higlowx/scal/ebpp/service/trade/AppStart.java
@@ -1,11 +1,9 @@
 package com.higlowx.scal.ebpp.service.trade;
 
-import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
